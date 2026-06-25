81d4b90e677985a9e540d75503d767f4a85d0c5a
jfrosorio <jfrosorio@gmail.com>
2026-06-25 12:04:20 +0100

    test(callbacks): cover MB and MBWay flows, models and events

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,4 +13,24 @@ abstract class TestCase extends Orchestra
             EuPagoServiceProvider::class,
         ];
     }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('app.key', 'base64:' . base64_encode(random_bytes(32)));
+
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        $app['config']->set('eupago.api_key', 'test-api-key');
+        $app['config']->set('eupago.channel', 'demo');
+    }
+
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
 }
