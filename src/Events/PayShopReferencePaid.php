60d2e18e45dd227b71d05e8e1f0b71d0bc00b8c8
jfrosorio <jfrosorio@gmail.com>
2026-06-26 15:36:36 +0100

    feat(payshop): add PayShop reference support

---
+++ src/Events/PayShopReferencePaid.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CodeTech\EuPago\Events;
+
+use CodeTech\EuPago\Models\PayShopReference;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PayShopReferencePaid
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The PayShop reference object.
+     *
+     * @var PayShopReference
+     */
+    public $reference;
+
+    /**
+     * PayShopReferencePaid constructor.
+     *
+     * @param PayShopReference $reference
+     */
+    public function __construct(PayShopReference $reference)
+    {
+        $this->reference = $reference;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
